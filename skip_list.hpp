78298e52117be9720318e806b3ad4c62bdd026c5
MaxUB <maxub@Ubuntu.myguest.virtualbox.org>
2025-05-17 15:41:36 +0300

    created test.cpp

--- skip_list.hpp
+++ skip_list.hpp
@@ -1,6 +1,9 @@
 #pragma once 
 
+#include <algorithm>
 #include <vector>
+
+using std::swap;
 
 template <typename key_T, typename T>
 class skip_list{
@@ -23,6 +26,70 @@ private:
    Node* node_by_key(const key_T& in_key);
    size_t new_level();
 public:
-	class iterator{}; 
-
+	class iterator{
+	  private:
+	  	skip_list* current_list;
+	  	Node* current_node;
+	  	friend class skip_list<key_T, T>;
+	  public:
+	  	iterator(const iterator& in_iterator);
+	  	iterator(skip_list& in_skip_list);
+	  	operator bool();
+	  	iterator& operator++();
+	  	iterator& operator--();
+	  	iterator& operator++(int);
+	  	iterator& operator--(int);
+	  	iterator& operator+=(size_t n);
+	  	iterator& operator-=(size_t n);
+	  	iterator& operator=(const iterator& in_iterator);
+	  	iterator& operator=(const key_T& in_key);
+	  	T& operator*();
+	  	void remove();
+	}; 
+	
+	skip_list(size_t in_num_of_lanes = 3, double in_skip_div= 1.0/4.0);
+	~skip_list();
+	iterator get_begin();
+	iterator get_end();
+	void push(const key_T& in_key, const T& in_data);
+	void remove(const key_T& in_key);
+	T& operator[](const key_T& in_key);
+	size_t count();
+	void clear();
+	iterator find(const T& unknown);
+	bool is_empty();
 };
+
+template <typename key_T, typename T>
+typename skip_list<key_T, T>::Node* skip_list<key_T, 
+T>::Node::previous_on_level(size_t in_level){
+	if (!this) return NULL;
+	Node* current = prev;
+	for (; current && !(current->next.count() >= in_level+1));
+	current = current->next[in_level - 1] {}
+	return current;
+}
+
+template <typename key_T, typename T>
+typename skip_list<key_T, T>::Node* skip_list<key_T, 
+T>::Node::next_on_level(size_t in_level){
+	if (!this) return NULL;
+	Node* current = next[in_level - 1];
+	for(; current && !(current-> next.count() >= in_level + 1));
+	current = current->next[in_level - 1] {}
+	return current;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
